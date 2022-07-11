b2103599f31a7cddb27ea34510859b9652482fab
tauqeerkhattak <17sw72@students.muet.edu.pk>
2022-07-11 22:05:22 +0500

    100 done

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import json
+from pathlib import Path
+
+
+base_url = "https://en.wikipedia.org"
+counter = 100
+error_birds = {"names": []}
+error_index = 0
 
 
 def scrap_bird_list():
@@ -13,33 +22,60 @@ def scrap_bird_list():
     page_elements = soup.find_all('li')
     print("No. of Birds: "+str(len(page_elements)))
     count = 0
-    data = {'birds': []}
+    birds_json = {'birds': []}
     with open("birds.json", "w") as outfile:
         index = 0
         for i in page_elements:
             count = count + 1
             if 27 < count < 11002:
                 print(i.text)
-                data['birds'].insert(index, i.text)
+                birds_json['birds'].insert(index, i.text)
                 index = index + 1
-        print(data)
-        outfile.write(json.dumps(data, indent=4))
+        print(birds_json)
+        outfile.write(json.dumps(birds_json, indent=4))
 
 
-def scrap_bird_image():
-    bird_name = "Yellow-scarfed tanager"
-    bird_name = bird_name.replace(" ", "_")
-    bird_name = bird_name.lower()
+def scrap_bird_image(bird_name):
+    bird_name = bird_name.replace(" ", "_").lower()
     print(bird_name)
-    wiki_page_url = "https://en.wikipedia.org/wiki/"+bird_name
-    image_url = "https://en.wikipedia.org"
-    page = requests.get(wiki_page_url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    image = soup.find('a', class_="image")
-    print(image_url+image.attrs["href"])
+    wiki_page_url = base_url + "/wiki/" + bird_name
+    wiki_page = requests.get(wiki_page_url)
+    soup = BeautifulSoup(wiki_page.content, "html.parser")
+    image_link = soup.find('a', class_="image")
+    try:
+        download_url = base_url + image_link.attrs["href"]
+        bird_image_page = requests.get(download_url)
+        soup = BeautifulSoup(bird_image_page.content, "html.parser")
+        bird_image = soup.find("div", id="file")
+        print("https:" + bird_image.next.attrs["href"])
+        save_image(bird_name, ".jpg", "https:" + bird_image.next.attrs["href"])
+    except AttributeError:
+        print("Attribute Error: "+bird_name)
+        error_birds["names"].insert(error_index, bird_name)
+
+
+def save_image(bird_name, extension, pic_url):
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+    image_content = requests.get(pic_url, headers=headers)
+    directory = "birds/"+bird_name
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    path = directory + "/" + bird_name + extension
+    print(path)
+    with open(path, "wb") as handle:
+        handle.write(image_content.content)
 
 
 if __name__ == '__main__':
-    scrap_bird_image()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open("birds.json", "r") as file:
+        data = json.load(file)
+        birds_data = data["birds"]
+        i = 0
+        while i < counter:
+            scrap_bird_image(birds_data[i])
+            time.sleep(4)
+            i = i + 1
+    with open("error.json", "w") as outfile:
+        outfile.write(json.dumps(error_birds, indent=4))
